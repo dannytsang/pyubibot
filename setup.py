eb95d0042a22f22aac5a17ebd87a660ebd241d13
Danny Tsang <danny@tsang.uk>
2022-06-28 22:28:03 +0100

    Bumped pypi build to 0.0.3.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyubibot",
-    version="0.0.2",
+    version="0.0.3",
     author="Danny Tsang",
     author_email="danny@tsang.uk",
     description="Python implementation for Ubibot APIs.",
