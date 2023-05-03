46c46ecbff8684f445f2e64aa016e24c7ef11912
Danny Tsang <danny@tsang.uk>
2023-05-03 19:47:26 +0100

    Updated packages to fix missing Python code.
    Setup was not including the Python directory.
    
    Fixes #2

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyubibot",
-    version="0.0.3",
+    version="0.0.4",
     author="Danny Tsang",
     author_email="danny@tsang.uk",
     description="Python implementation for Ubibot APIs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dannytsang/pyubibot",
-    packages=setuptools.find_packages(where="pyubibot"),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
